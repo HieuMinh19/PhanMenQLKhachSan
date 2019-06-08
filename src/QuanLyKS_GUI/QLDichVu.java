78a37424ad2cde33fb10153289d9ea64ae808254
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2019-06-08 14:13:29 +0700

    update and delete Dichvu

--- src/QuanLyKS_GUI/QLDichVu.java
+++ src/QuanLyKS_GUI/QLDichVu.java
@@ -9,15 +9,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
+import QuanLyKS_DTO.ChucVu_DTO;
 import QuanLyKS_DTO.DichVu_DTO;
+import QuanLyKS_DTO.NhanVien_DTO;
 import net.proteanit.sql.DbUtils;
 
 import QuanLyKS_BUS.DichVu_BUS;
+import QuanLyKS_BUS.NhanVien_BUS;
+
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JComboBox;
@@ -31,13 +41,15 @@ public class QLDichVu extends JInternalFrame {
 	private static final long serialVersionUID = 1L;
 	private JTable table_2;
 	private JTextField txtTenDichVu;
-
+	private JTable table;
 	ArrayList<DichVu_DTO> listDV_DTO =  new ArrayList<DichVu_DTO>();
 	static DichVu_BUS dvBus = new DichVu_BUS();	
 	public static ResultSet DV = dvBus.selectAll(); 
 	private DichVu_DTO dichvu_DTO;
 	private JTextField txtGiaDichVu;
-	
+	private DichVu_BUS bus;
+	private ArrayList<DichVu_DTO> dsnv;
+	private int selectedMaNhanVien;
 	/**
 	 * Launch the application.
 	 */
@@ -58,117 +70,110 @@ public class QLDichVu extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public QLDichVu() {
-		setBounds(100, 100, 605, 315);
+		bus=new DichVu_BUS();
+		dsnv = new ArrayList<DichVu_DTO>();
+		setBounds(100, 100, 768, 480);
 		getContentPane().setLayout(null);
-
 		JScrollPane scrListDV = new JScrollPane();
-		JComboBox<Object> cbxMaDV = new javax.swing.JComboBox<>();
-		cbxMaDV.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent arg0) {
-				int idDV = Integer.parseInt( ( (String) cbxMaDV.getSelectedItem() ));
-				for(int i = 0; i < listDV_DTO.size(); i++) {
-        			DichVu_DTO dvCompare = listDV_DTO.get(i);
-        			if(idDV == dvCompare.getMaDichVu())
-        				txtTenDichVu.setText(dvCompare.getTenDichVu());
-        				txtGiaDichVu.setText( String.valueOf(dvCompare.getGiaDichVu() ) );
-        		}
-			}
-		});
-		
 		scrListDV.addMouseListener(new MouseAdapter() {
-			 
 			public void mouseClicked(MouseEvent e) {
 			
 			}
 		});
-		scrListDV.setBounds(276, 40, 270, 163);
+		scrListDV.setBounds(59, 40, 693, 189);
 		getContentPane().add(scrListDV);
 		
-		table_2 = new JTable();
-		scrListDV.setViewportView(table_2);
-		
+		DefaultTableModel m = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Mã Dịch Vụ", "Tên Dịch Vụ", "Giá Dịch Vụ"
+				}
+			);
+		table = new JTable(m);
+		ListSelectionModel cellSelectionModel = table.getSelectionModel();
+	    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {	
+				int[] selectedRowColums = table.getSelectedRows();	
+			    for (int i = 0; i < selectedRowColums.length; i++) {
+			    	selectedMaNhanVien = (int) table.getValueAt(selectedRowColums[i], 0);
+			        txtTenDichVu.setText((String) table.getValueAt(selectedRowColums[i], 1));
+			        txtGiaDichVu.setText(table.getValueAt(selectedRowColums[i], 2).toString());
+			    }		
+			  }
+        });
+	    scrListDV.setViewportView(table);
 		JButton btnLoadData = new JButton("Load list Service");
 		btnLoadData.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				ResultSet listDV = dvBus.selectAll();
-				ResultSet subList = dvBus.selectAll();
-								
-				if(listDV != null) {
-					table_2.setModel(DbUtils.resultSetToTableModel(listDV));
-				}else {
-					JOptionPane.showMessageDialog(null, "get List Services not success", "Error: " + "error Mesage", JOptionPane.CLOSED_OPTION);
+				if (m.getRowCount() > 0) {
+				    for (int i = m.getRowCount() - 1; i > -1; i--) {
+				        m.removeRow(i);
+				    }
 				}
-				//load list MaDV from db
-				 try {
-			        	while(subList.next()) {
-			        		DichVu_DTO dvItem = new DichVu_DTO(subList.getInt("MaDichVu"), subList.getString("TenDichVu"),subList.getInt("GiaDichVu"));
-			        		listDV_DTO.add(dvItem);  		
-			        		cbxMaDV.addItem(subList.getString("MaDichVu"));
-			        		cbxMaDV.getSelectedItem();			        		
-					   }		  
-				} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				dsnv = bus.LoadListDV();
+				dsnv.forEach(nv -> m.addRow(new Object[]{nv.getMaDichVu(), nv.getTenDichVu(),nv.getGiaDichVu()}));
 			}
 		});
-		btnLoadData.setBounds(341, 214, 109, 30);
+		btnLoadData.setBounds(331, 229, 150, 37);
 		getContentPane().add(btnLoadData);
 		
-		JLabel lblMadichvu = new JLabel("M\u00E3 d\u1ECBch v\u1EE5");
-		lblMadichvu.setBounds(27, 59, 59, 14);
-		getContentPane().add(lblMadichvu);
-		
 		JLabel lblTnDchVu = new JLabel("T\u00EAn d\u1ECBch v\u1EE5");
-		lblTnDchVu.setBounds(27, 101, 59, 14);
+		lblTnDchVu.setBounds(144, 282, 78, 14);
 		getContentPane().add(lblTnDchVu);
 		
 		txtTenDichVu = new JTextField();
-		txtTenDichVu.setBounds(96, 98, 142, 20);
+		txtTenDichVu.setBounds(273, 279, 226, 20);
 		getContentPane().add(txtTenDichVu);
 		txtTenDichVu.setColumns(10);
 		
 		JButton btnUpdate = new JButton("Update");
 		btnUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dichvu_DTO = new DichVu_DTO( ); 
-
-				dichvu_DTO.setTenDichVu( txtTenDichVu.getText() );
-				dichvu_DTO.setGiaDichVu( Integer.parseInt(txtGiaDichVu.getText()));
-					 if(  DichVu_BUS.Update(dichvu_DTO) == true)
-						 JOptionPane.showMessageDialog(null, "Update Dich Vu Success", "Success: " + "Success Mesage", JOptionPane.INFORMATION_MESSAGE);
-					 else
-						 JOptionPane.showMessageDialog(null, "Update Dich Vu Fail", "Fail: " + "Success Mesage", JOptionPane.CLOSED_OPTION);
+			Integer GiaDichVu =	Integer.parseInt(txtGiaDichVu.getText());
+				boolean result = DichVu_BUS.Update(new DichVu_DTO(selectedMaNhanVien, txtTenDichVu.getText(),GiaDichVu ));
+				if(result) {
+					if (m.getRowCount() > 0) {
+					    for (int i = m.getRowCount() - 1; i > -1; i--) {
+					        m.removeRow(i);
+					    }
+					}
+					dsnv = bus.LoadListDV();
+					dsnv.forEach(nv -> m.addRow(new Object[]{nv.getMaDichVu(), nv.getTenDichVu(),nv.getGiaDichVu()}));
+				}
 
 			}
 		});
-		btnUpdate.setBounds(27, 190, 89, 23);
+		btnUpdate.setBounds(194, 362, 109, 37);
 		getContentPane().add(btnUpdate);
-		
-		cbxMaDV.setBounds(96, 56, 142, 20);
-		getContentPane().add(cbxMaDV);
 
 		txtGiaDichVu = new JTextField();
 		txtGiaDichVu.setColumns(10);
-		txtGiaDichVu.setBounds(96, 144, 142, 20);
+		txtGiaDichVu.setBounds(275, 312, 224, 20);
 		getContentPane().add(txtGiaDichVu);
 
 		JLabel lblGiaDichVu = new JLabel("Gia Dich Vu");
-		lblGiaDichVu.setBounds(27, 147, 109, 14);
+		lblGiaDichVu.setBounds(144, 315, 109, 14);
 		getContentPane().add(lblGiaDichVu);
 
 		JButton btndelete = new JButton("Delete");
 		btndelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				dichvu_DTO = new DichVu_DTO( ); 		
-					 if(  DichVu_BUS.Delete(dichvu_DTO) == true)
-						 JOptionPane.showMessageDialog(null, "Update Dich Vu Success", "Success: " + "Success Mesage", JOptionPane.INFORMATION_MESSAGE);
-					 else
-						 JOptionPane.showMessageDialog(null, "Update Dich Vu Fail", "Fail: " + "Success Mesage", JOptionPane.CLOSED_OPTION);
+				boolean result = DichVu_BUS.Delete(selectedMaNhanVien);
+				if(result) {
+					if (m.getRowCount() > 0) {
+					    for (int i = m.getRowCount() - 1; i > -1; i--) {
+					        m.removeRow(i);
+					    }
+					}
+					dsnv = bus.LoadListDV();
+					dsnv.forEach(nv -> m.addRow(new Object[]{nv.getMaDichVu(), nv.getTenDichVu(),nv.getGiaDichVu()}));
+				}
 			}
 		});
-		btndelete.setBounds(126, 190, 109, 23);
+		btndelete.setBounds(403, 362, 125, 37);
 		getContentPane().add(btndelete);
 	}
 }
