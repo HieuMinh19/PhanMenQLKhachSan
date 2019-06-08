d9b49f2cda69d9fe2e87f5b3585317521ec0cef0
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2019-06-08 13:07:22 +0700

    Insert Thanh Toan cua table HoaDon

--- src/QuanLyKS_GUI/frmThanhToan.java
+++ src/QuanLyKS_GUI/frmThanhToan.java
@@ -4,14 +4,21 @@
 	import javax.swing.ListSelectionModel;
 	import javax.swing.JInternalFrame;
 	import javax.swing.JLabel;
-	import java.awt.Font;
+	import java.sql.Date;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
 	import javax.swing.JTextPane;
 	import javax.swing.ListSelectionModel;
 	import javax.swing.table.DefaultTableModel;
-	import java.lang.NumberFormatException;
+
+	import com.ibm.icu.text.Edits.Iterator;
+
+import java.lang.NumberFormatException;
 	import QuanLyKS_BUS.NhanVien_BUS;
 	import QuanLyKS_DTO.HoaDon_DTO;
-	import QuanLyKS_BUS.HoaDon_BUS;
+import QuanLyKS_DTO.NhanVien_DTO;
+import QuanLyKS_BUS.HoaDon_BUS;
 	import javax.swing.JButton;
 	import javax.swing.JScrollPane;
 	import javax.swing.JTable;
@@ -25,8 +32,11 @@
 	import java.text.SimpleDateFormat;
 	
 	import java.util.concurrent.TimeUnit;
+import java.awt.Button;
 	public class frmThanhToan extends JInternalFrame {
-	
+		int tienPhong =0;
+		int tiendichVu =0;
+		int tongtien =0;
 		private HoaDon_BUS bus;
 		private JTable table;
 		private JTable table_dich;
@@ -56,19 +66,25 @@
 		 */
 		public frmThanhToan() {
 			
+			ArrayList<Integer> TienPhong = new ArrayList<>();
+			ArrayList<Integer> TienDichVu = new ArrayList<>();
 			bus=new HoaDon_BUS();
 			
 			setBounds(100, 100, 972, 692);
 			getContentPane().setLayout(null);
 			
+			JTextPane txtTongTienThanhToan = new JTextPane();
+			txtTongTienThanhToan.setBounds(792, 430, 139, 36);
+			getContentPane().add(txtTongTienThanhToan);
 			
-			JTextPane txtTongTien = new JTextPane();
-			txtTongTien.setBounds(738, 367, 139, 49);
-			getContentPane().add(txtTongTien);
+			
+			JTextPane txtTongTienPhong = new JTextPane();
+			txtTongTienPhong.setBounds(792, 332, 139, 36);
+			getContentPane().add(txtTongTienPhong);
 			//txtTongTien.setText("ds");
-			JLabel lblTngTin = new JLabel("Tá»•ng Tiá»�n");
-			lblTngTin.setFont(new Font("Tahoma", Font.PLAIN, 18));
-			lblTngTin.setBounds(620, 367, 102, 49);
+			JLabel lblTngTin = new JLabel("T\u1ED5ng Ti\u1EC1n Ph\u00F2ng");
+			lblTngTin.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblTngTin.setBounds(580, 332, 142, 49);
 			getContentPane().add(lblTngTin);
 			
 			JLabel lblInvoicePayment = new JLabel("Thanh Toan");
@@ -79,6 +95,10 @@
 			JTextPane txtPhong = new JTextPane();
 			txtPhong.setBounds(334, 88, 102, 29);
 			getContentPane().add(txtPhong);
+			
+			JTextPane txtTongTienDichVu = new JTextPane();
+			txtTongTienDichVu.setBounds(792, 381, 139, 36);
+			getContentPane().add(txtTongTienDichVu);
 			
 			JLabel lblNewLabel = new JLabel("Room");
 			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -97,22 +117,22 @@
 					}
 				);
 				table = new JTable(m);
-				ListSelectionModel cellSelectionModel = table.getSelectionModel();
-			    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-					public void valueChanged(ListSelectionEvent e) {
-					    int[] selectedRow = table.getSelectedRows();
-					    for (int i = 0; i < selectedRow.length; i++) {
-					    	startDate =	(Date) table.getValueAt(selectedRow[i], 4);
-					        endDate = (Date) table.getValueAt(selectedRow[i], 5);
-					        long getDiff = endDate.getTime() - startDate.getTime();
-					        long getDaysDiff = getDiff / (24 * 60 * 60 * 1000);
-					        giatien = table.getValueAt(selectedRow[i], 3).toString()  ;
-					        giatien1 =  getDaysDiff * Long.parseLong(giatien) ;				      
-					        txtTongTien.setText(String.valueOf(giatien1));
-					    }
-					  }
-		        });
+//				ListSelectionModel cellSelectionModel = table.getSelectionModel();
+//			    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//			    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+//					public void valueChanged(ListSelectionEvent e) {
+//					    int[] selectedRow = table.getSelectedRows();
+//					    for (int i = 0; i < selectedRow.length; i++) {
+//					    	startDate =	(Date) table.getValueAt(selectedRow[i], 4);
+//					        endDate = (Date) table.getValueAt(selectedRow[i], 5);
+//					        long getDiff = endDate.getTime() - startDate.getTime();
+//					        long getDaysDiff = getDiff / (24 * 60 * 60 * 1000);
+//					        giatien = table.getValueAt(selectedRow[i], 3).toString()  ;
+//					        giatien1 =  getDaysDiff * Long.parseLong(giatien) ;				      
+//					        txtTongTien.setText(String.valueOf(giatien1));
+//					    }
+//					  }
+//		        });
 				scrlistthanhtoan.setViewportView(table);
 				JScrollPane scrlisttendichvu = new JScrollPane();
 				scrlisttendichvu.setBounds(12, 332, 556, 98);
@@ -136,7 +156,7 @@
 					        m.removeRow(i);
 					    }
 					};
-					int a ;
+					
 					ArrayList<HoaDon_DTO> dsnv = bus.SearchNV(Integer.parseInt(txtPhong.getText()));
 					dsnv.forEach(nv -> m.addRow(new Object[]{
 							nv.getMaCTDatPhong(), 
@@ -145,7 +165,8 @@
 							nv.getGiaPhong(),
 							nv.getNgayNhan(),
 							nv.getNgayTra()
-							}));				
+							}));
+					//
 					if (n.getRowCount() > 0) {
 					    for (int i = n.getRowCount() - 1; i > -1; i--) {
 					        n.removeRow(i);
@@ -157,24 +178,103 @@
 							nv.getSoLuong(), 
 							nv.getTongTienDichVu()
 							}));
-					
+				    /////
+					dsnv.forEach((element) -> {
+				         // a =  element.getMaCTDatPhong();
+//				          System.out.println("ma pg"+a);
+//				            System.out.println(element.getHoTen());
+//				            System.out.println(element.getTenLoaiPhong());
+//				            System.out.println(element.getGiaPhong());
+//				            System.out.println(element.getNgayNhan());
+//				            System.out.println(element.getNgayTra());
+				            startDate = element.getNgayNhan();
+				            System.out.println("Ngay Nhan"+startDate);
+				            endDate = element.getNgayTra();
+				            System.out.println("Ngay Tra"+endDate);
+				            long getDiff = endDate.getTime() - startDate.getTime();
+				            System.out.println("so ngay"+getDiff);
+				            long getDaysDiff = getDiff / (24 * 60 * 60 * 1000);
+				            System.out.println("ngay"+getDaysDiff);
+				            tienPhong +=  getDaysDiff * element.getGiaPhong();	
+				            System.out.println("tien phogn"+tienPhong);
+				            //startDate =	(Date) table.getValueAt(selectedRow[i], 4);
+					      //  endDate = (Date) table.getValueAt(selectedRow[i], 5);
+					       // long getDiff = endDate.getTime() - startDate.getTime();
+					       // long getDaysDiff = getDiff / (24 * 60 * 60 * 1000);
+					       // giatien = table.getValueAt(selectedRow[i], 3).toString()  ;
+					       // giatien1 =  getDaysDiff * Long.parseLong(giatien) ;				      
+					       // txtTongTien.setText(String.valueOf(giatien1));
+				           txtTongTienPhong.setText(String.valueOf(tienPhong));
+				        });
+					tendv.forEach((element) -> {
+			         // a =  element.getMaCTDatPhong();
+//			          System.out.println("ma pg"+a);
+//			            System.out.println(element.getHoTen());
+//			            System.out.println(element.getTenLoaiPhong());
+//			            System.out.println(element.getGiaPhong());
+//			            System.out.println(element.getNgayNhan());
+//			            System.out.println(element.getNgayTra());
+						tiendichVu += element.getTongTienDichVu();
+			            //startDate =	(Date) table.getValueAt(selectedRow[i], 4);
+				      //  endDate = (Date) table.getValueAt(selectedRow[i], 5);
+				       // long getDiff = endDate.getTime() - startDate.getTime();
+				       // long getDaysDiff = getDiff / (24 * 60 * 60 * 1000);
+				       // giatien = table.getValueAt(selectedRow[i], 3).toString()  ;
+				       // giatien1 =  getDaysDiff * Long.parseLong(giatien) ;				      
+				       // txtTongTien.setText(String.valueOf(giatien1));
+						txtTongTienDichVu.setText(String.valueOf(tiendichVu));
+			        });
+					txtTongTienThanhToan.setText(String.valueOf(tiendichVu+tienPhong));
+					tienPhong = 0;
+					tiendichVu = 0;
+					tongtien = 0;
+				}
 				
-				    }
 			});
 			btnSearchThanhToan.setFont(new Font("Tahoma", Font.PLAIN, 18));
 			btnSearchThanhToan.setBounds(620, 88, 102, 29);
 			getContentPane().add(btnSearchThanhToan);
 			
-			JTextPane txtstartDate = new JTextPane();
-			txtstartDate.setBounds(630, 433, 139, 49);
-			getContentPane().add(txtstartDate);
+			JLabel lblTngTinDich = new JLabel("T\u1ED5ng Ti\u1EC1n Dich Vu");
+			lblTngTinDich.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblTngTinDich.setBounds(580, 375, 164, 49);
+			getContentPane().add(lblTngTinDich);
 			
+			JLabel lblTngTinThanh = new JLabel("T\u1ED5ng Ti\u1EC1n Thanh To\u00E1n");
+			lblTngTinThanh.setFont(new Font("Tahoma", Font.PLAIN, 18));
+			lblTngTinThanh.setBounds(580, 425, 183, 49);
+			getContentPane().add(lblTngTinThanh);
 			
-		    
+			Button btnThanhToan = new Button("Thanh To\u00E1n");
+			btnThanhToan.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					String SoTienThu = txtTongTienThanhToan.getText();
+					int SoTienThu1 = Integer.parseInt(SoTienThu);
+					System.out.println("So tien thu" + " "+SoTienThu);
+					String SoPhong = txtPhong.getText();
+					int SoPhong1 = Integer.parseInt(SoPhong);
+					System.out.println("So Phong" + " "+SoPhong);
+				int MaNhanVien = frmDashboard.user.getMaNhanVien();
+					
+					System.out.println("So tien thu" + " "+MaNhanVien);
+					
+					
+					System.out.println("Ngay Hien Tại localDate"+" "+java.time.LocalDate.now()); 
+					 long millis = System.currentTimeMillis();
+					 java.sql.Date date = new java.sql.Date(millis);
+					 System.out.println("Ngay Hien Tại Date"+" "+date); 
+
+					
+         		HoaDon_DTO nvDTO = new HoaDon_DTO(date,SoTienThu1,SoPhong1,MaNhanVien);
+	        		if(HoaDon_BUS.Insert(nvDTO) == true) {
+	        			JOptionPane.showMessageDialog(null, "Thanh Toan Thanh Cong", "Success: " + "Success Mesage", JOptionPane.INFORMATION_MESSAGE);
+	        		}else {
+	        			JOptionPane.showMessageDialog(null, "Thanh Toan That Bai", "Fail: " + "Success Mesage", JOptionPane.CLOSED_OPTION);
+	        		}
+			}
+			});
+			btnThanhToan.setBounds(522, 527, 164, 24);
+			getContentPane().add(btnThanhToan);
 			
-			
-			
-			
-	
 		}
 	}
